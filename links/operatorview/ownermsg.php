0ee39d4fadfa5320a9e4aac116f62a6efc90d7da
nagamoto220 <75156131+nagamoto220@users.noreply.github.com>
2020-12-04 23:48:56 +0800

    Update ownermsg.php

--- links/operatorview/ownermsg.php
+++ links/operatorview/ownermsg.php
@@ -1,4 +1,4 @@
-<?php session_start(); include_once('../config.php'); include('../paginator.class.php'); include('addreservation.php'); $userpaymentID  = $_SESSION['id'];?>
+<?php session_start(); include_once('../config.php'); include('../paginator.class.php'); $userpaymentID  = $_SESSION['id'];?>
 <!doctype html>
 <html lang="en-US" xmlns:fb="https://www.facebook.com/2008/fbml" xmlns:addthis="https://www.addthis.com/help/api-spec"  prefix="og: http://ogp.me/ns#" class="no-js">
 <head>
