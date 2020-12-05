a3d0dd0a902fd9608cc883b444eec4edc6a5389a
nagamoto220 <75156131+nagamoto220@users.noreply.github.com>
2020-12-06 01:29:54 +0800

    Update operatormsg.php

--- links/userview/operatormsg.php
+++ links/userview/operatormsg.php
@@ -1,4 +1,13 @@
-<?php session_start(); include_once('../config.php'); include('../paginator.class.php'); include('addreservation.php'); $userpaymentID  = $_SESSION['id'];?>
+<?php session_start(); include_once('../config.php'); include('../paginator.class.php'); include('addreservation.php'); $userpaymentID  = $_SESSION['id'];
+$result = $mysqli->query("SELECT * FROM useraccount WHERE id='$userpaymentID'");
+$val = $result->fetch_assoc();
+$TID =  $val['Tenant_Id'];
+
+$resultss = $mysqli->query("SELECT * FROM tenant WHERE Tenant_Id='$TID'");
+$vals = $results->fetch_assoc();
+$TN =  $val['Tenant_Name'];
+
+?>
 <!doctype html>
 <html lang="en-US" xmlns:fb="https://www.facebook.com/2008/fbml" xmlns:addthis="https://www.addthis.com/help/api-spec"  prefix="og: http://ogp.me/ns#" class="no-js">
 <head>
@@ -88,7 +97,7 @@
 		$pages->mid_range = 9;
 		$pages->paginate();
 
-		$result	=	$mysqli->query("SELECT * FROM tenan_message where location ='operator' ORDER BY ID ASC ".$pages->limit."");
+		$result	=	$mysqli->query("SELECT * FROM tenan_message where location ='operator' AND tenant_name='$TN' ORDER BY ID ASC ".$pages->limit."");
 }
 	?>
 	<div class="clearfix"></div>
