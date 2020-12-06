fca946f6ff46d4e2ae9df314423640d18ca8b775
Unohana13 <74942125+Unohana13@users.noreply.github.com>
2020-12-06 10:54:03 +0800

    Update PHPMailer.php

--- PHPMailer-master/src/PHPMailer.php
+++ PHPMailer-master/src/PHPMailer.php
@@ -424,7 +424,7 @@ class PHPMailer
      *
      * @var string|callable|\Psr\Log\LoggerInterface
      */
-    public $Debugoutput = 'echo';
+    // public $Debugoutput = 'echo';
 
     /**
      * Whether to keep SMTP connection open after each message.
